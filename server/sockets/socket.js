896ea443c9e0091eb78a02215a2ee7715e5632bc
Pablo Cremades Serra <pablo@MacBook-Pro-de-Pablo.local>
2020-05-12 13:45:14 +0200

    Fist commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,39 @@
+const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl();
+
+io.on('connection', (client) => {
+
+    // Escuchar el cliente
+    client.on('siguienteTicket', (data, callback) => {
+        let siguiente = ticketControl.siguiente();
+        client.emit('siguienteTicket', siguiente);
+        callback(siguiente);
+    });
+
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    });
+
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        client.broadcast.emit('estadoActual', {
+            ultimos4: ticketControl.getUltimos4()
+        });
+
+        callback(atenderTicket);
+
+        // actualizar/ notificar cambios en los ULTIMOS 4
+    });
+
+});
